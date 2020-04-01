526453e5aa4d5a81d8815023000f6db193659f7f
Panumas Srisook (Moss) <panumas.s@thtal.com>
2020-04-01 13:21:17 +0700

    end mern

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -33,24 +33,50 @@ export const GlobleProvider = ({ children }) => {
     }
   }
 
-  function deleteTransection(id) {
-    dispatch({
-      type: "DELETE_TRANSACTION",
-      payload: id
-    });
+  async function deleteTransection(id) {
+    try {
+      await axios.delete(`/api/v1/transactions/${id}`);
+
+      dispatch({
+        type: "DELETE_TRANSACTION",
+        payload: id
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTIONS_ERROR",
+        payload: err.response.data.error
+      });
+    }
   }
 
-  function addTransection(transaction) {
-    dispatch({
-      type: "ADD_TRANSACTION",
-      payload: transaction
-    });
+  async function addTransection(transaction) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.post("/api/v1/transactions", transaction, config);
+
+      dispatch({
+        type: "ADD_TRANSACTION",
+        payload: res.data.data
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTIONS_ERROR",
+        payload: err.response.data.error
+      });
+    }
   }
 
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
         deleteTransection,
         addTransection
       }}
